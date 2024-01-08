ad39809c206bfeb4a9662787b2f120df722280d2
Abdulbasit Saeed <abdulbasitsaeed619@gmail.com>
2024-01-08 21:46:16 +0100

    Update images and descriptions for contentData

--- assets/js/dynamic-list.js
+++ assets/js/dynamic-list.js
@@ -3,52 +3,38 @@ const contentListContainer = document.getElementById('home-second-causes');
 
 const contentData = [
     {
-        imgUrl: 'assets/images/bg_1.jpeg',
-        raisedAmount: '$10086 raised of $15000',
-        percent: '67%',
-        tag: 'Education',
-        title: 'Education for Poor Childrens',
-        description: 'Inventore veritatis et quasi archit see ecto beatae vitae dicta sunt explaemo enim voluptatem quia',
-        link: 'causes-single.html'
+        imgUrl: 'assets/images/orphanage_project.jpeg', // Update with an appropriate image
+        tag: 'Orphanage',
+        title: 'Support for Orphanage Project',
+        description: 'Help us build a self-sustaining community including a children’s home, school, and other facilities to support orphans and destitute children in Nigeria.',
     },
     {
-        imgUrl: 'assets/images/bg_1.jpeg',
-        raisedAmount: '$10086 raised of $15000',
-        percent: '67%',
+        imgUrl: 'assets/images/education_project.jpeg', // Update with an appropriate image
         tag: 'Education',
-        title: 'Education for Poor Childrens',
-        description: 'Inventore veritatis et quasi archit see ecto beatae vitae dicta sunt explaemo enim voluptatem quia',
-        link: 'causes-single.html'
+        title: 'Education for Children',
+        description: 'Contribute to providing education, vocational training, and holistic development for street children in Nigeria.',
     },
     {
-        imgUrl: 'assets/images/bg_1.jpeg',
-        raisedAmount: '$10086 raised of $15000',
-        percent: '67%',
-        tag: 'Education',
-        title: 'Education for Poor Childrens',
-        description: 'Inventore veritatis et quasi archit see ecto beatae vitae dicta sunt explaemo enim voluptatem quia',
-        link: 'causes-single.html'
+        imgUrl: 'assets/images/healthcare_project.jpeg', // Update with an appropriate image
+        tag: 'Healthcare',
+        title: 'Healthcare for Vulnerable Children',
+        description: 'Support our mission to offer healthcare services to vulnerable children, including regular check-ups, medicines, and nutritional support.',
     },
     {
-        imgUrl: 'assets/images/bg_1.jpeg',
-        raisedAmount: '$10086 raised of $15000',
-        percent: '67%',
+        imgUrl: 'assets/images/vocational_training.jpeg', // Update with an appropriate image
         tag: 'Education',
-        title: 'Education for Poor Childrens',
-        description: 'Inventore veritatis et quasi archit see ecto beatae vitae dicta sunt explaemo enim voluptatem quia',
-        link: 'causes-single.html'
+        title: 'Education for Children',
+        description: 'Contribute to providing education, vocational training, and holistic development for street children in Nigeria.',
     },
     {
-        imgUrl: 'assets/images/bg_1.jpeg',
-        raisedAmount: '$10086 raised of $15000',
-        percent: '67%',
-        tag: 'Education',
-        title: 'Education for Poor Childrens',
-        description: 'Inventore veritatis et quasi archit see ecto beatae vitae dicta sunt explaemo enim voluptatem quia',
-        link: 'causes-single.html'
-    },
-    // Add more objects here
+        imgUrl: 'assets/images/clothing_project.jpeg', // Replace with an actual image relevant to the clothing project
+        tag: 'Clothing',
+        title: 'Clothing for Needy Children',
+        description: 'Support our initiative to provide clothing for underprivileged children, ensuring they have adequate attire for all seasons and occasions.',
+    }
+    
 ];
+
 
 contentData.forEach(data => {
     const itemDiv = document.createElement('div');
@@ -57,18 +43,8 @@ contentData.forEach(data => {
     itemDiv.innerHTML = `
         <div class="causes-wrap">
             <div class="img-wrap">
-                <a href="${data.link}"><img src="${data.imgUrl}" alt=""></a>
-                <div class="raised-progress">
-                    <div class="skillbar-wrap">
-                        <div class="clearfix">
-                            ${data.raisedAmount}
-                        </div>
-                        <div class="skillbar" data-percent="${data.percent}">
-                            <div class="skillbar-percent">${data.percent}</div>
-                            <div class="skillbar-bar"></div>
-                        </div>             
-                    </div>
-                </div>
+                <a href="${data.link}"><img src="${data.imgUrl}" alt="" class ="slide-image"></a>
+       
             </div>
 
             <div class="content-wrap">
@@ -76,7 +52,7 @@ contentData.forEach(data => {
                 <h3><a href="${data.link}">${data.title}</a></h3>
                 <p>${data.description}</p>
                 <div class="btn-wrap">
-                    <a class="btn-primary btn" href="${data.link}">Donate Now</a>
+                    <a class="btn-primary btn" href="#">Donate Now</a>
                 </div>
             </div>
         </div>
